ef1e471c528ee03040aa741e06ec073a6ece238d
s.shimko <sergey1shimko@gmail.com>
2016-09-03 20:04:20 +0300

    Bonus tasks partly

--- tasks/1_main_data_classes.rb
+++ tasks/1_main_data_classes.rb
@@ -29,14 +29,21 @@ end
 # strings are definitely different
 # strings can completely match
 # найти кол-во эл-ов которые совпадают подряд начиная сначала, посимвольно 2 строки сравнивать и считать совпадения
-#
-# first_string = "first and last"
-# second_string = "first and last"
-# # puts first_string.eql? second_string
-#
-# str1 = first_string.split('')
-# str2 = second_string.split('')
-# puts str1[0].eql? str2[0]
+def string_comparison
+  same_string_1 = "same strings"
+  same_string_2 = "same strings"
+  ss1 = same_string_1.split('')
+  ss2 = same_string_2.split('')
+  match_count_1 = ss1.count { |e| ss2.include? e }
+  puts match_count_1
+
+  diff_string_1 = "abcdefg"
+  diff_string_2 = "hjklmnop"
+  ds1 = diff_string_1.split('')
+  ds2 = diff_string_2.split('')
+  match_count_2 = ds1.size - (ds1 - ds2).size
+  puts match_count_2
+end
 
 
 # 5. There is an array of integers. First puts elements with even indexes and then with odd indexes
@@ -56,7 +63,7 @@ end
 # 7. There is an array of integers (ary). Modify it with adding ary[0] (first element of the array) to each even number.
 # Don't do it for first and last elements
 
-def avalue.to_idding_to_each_even_number(ary = [10, 21, 30, 41, 50, 61, 70, 81, 90])
+def adding_to_each_even_number(ary = [10, 21, 30, 41, 50, 61, 70, 81, 90])
   cut_ary = ary.select { |el| el != ary[0] && el != ary[-1] }
   even_cut_ary = cut_ary.select { |el| el.even? }
   puts even_cut_ary.map { |el| el + ary[0]}
@@ -88,7 +95,7 @@ def remove_pairs_start_with_s
 end
 
 # 10. There is a hash where keys are symbols and values are integers. Modify it: leave only pairs where value is
-natural number and more than 0
+# natural number and more than 0
 def leave_pairs_natural_and_more_than_zero
   h = {key1: 0,
        key2: 1,
@@ -99,100 +106,4 @@ def leave_pairs_natural_and_more_than_zero
   puts new_h
 end
 
-
-
-# Create method which gets 10 random integers (from 0 to 9) (generate it using while or until)
-#
-#                   before output for each next number we should puts "========="
-#                   if number is even or odd it should puts string "the number is even" or "the number is odd"
-#                   if it's more (or less) than 5 it should puts appropriate string "the number is more (or less) than 5"
-#                   in case we've got "0" we should puts "we've got 0"
-#                   in each case we should get 2 messages except the '0' case
-
-#                   2. add to your solution next test
-#
-#                   create project
-#                   make a random action: create or not a new bug issue
-#                   open project page,
-#                   open issues tab
-#                   if there is a bug present then add self to watchers
-#                   if no then create a new bug issue and then add self to watchers
-#                   Verify that there is a bug issue and that current user is a watcher
-#
-# def generate_ten_random_numbers
-#   i = 1
-#   while i <= 10
-#     a = rand(0..9)
-#     puts "================="
-#     puts "#{i}: a = #{a}"
-#
-#     if a < 5
-#       puts "the number is less than 5"
-#     elsif a == 5
-#       puts "the number is 5"
-#     else
-#       puts "the number is more than 5"
-#     end
-#     a.even? ? (puts "the number is even") : (puts "the number is odd")
-#     if a == 0
-#       puts "we've got 0"
-#     end
-#
-#     i += 1
-#   end
-# end
-
-# for i in 0..10 do
-#   puts i
-# end
-
-# i = 10
-# loop do
-#   break if i == 0
-#   puts i
-#   i -= 1
-# end
-#
-# class Animal
-#   def initialize
-#     @dog = 'dog'
-#     @cat = 'cat'
-#     @mouse = 'mouse'
-#   end
-#
-#   def sound(voice)
-#     puts "This bitch do #{voice}"
-#   end
-# end
-
-# var = lambda do
-#   puts "adsf"
-# end
-#
-# var.call
-
-
-
-# def call_yield
-#   ary = ['string', 'sss']
-#   yield(ary)
-# end
-#
-# call_yield { |var| puts var }
-
-#
-# def create_method(method_name)
-#   define_method(method_name.to_sym) do |a|
-#     puts "#{method_name} is defined"
-#     puts "#{a} is argument"
-#   end
-# end
-#
-# class User
-#   attr_reader :first_name, :last_name
-#
-#   attr_writer :first_name, :last_name
-#
-#   attr_accessor :first_name, :last_name
-# end
 
--- tasks/testbonustasks.rb
+++ tasks/testbonustasks.rb
@@ -7,48 +7,104 @@ class TestBonusTasks < Test::Unit::TestCase
 
   def setup
     @browser = Watir::Browser.new :chrome
+    @browser.driver.manage.timeouts.implicit_wait = 1
 
-    @login = ('testlogin1' + rand(9999999).to_s)
-    @login2 = ('testlogin2' + rand(9999999).to_s)
-    @login3 = ('testlogin3' + rand(9999999).to_s)
-    @project = ('test_project' + rand(9999999).to_s)
-
-    @pass = '1234'
-    @npass = '1111'
-    @pass2 = '1234'
+    # @login = ('testlogin1' + rand(9999999).to_s)
+    # @login2 = ('testlogin2' + rand(9999999).to_s)
+    # @login3 = ('testlogin3' + rand(9999999).to_s)
+    # @project = ('test_project' + rand(9999999).to_s)
+    #
+    # @pass = '1234'
+    # @npass = '1111'
+    # @pass2 = '1234'
   end
 
 # include TestRedmineMethods
 
-  def registration3(login, first_name, last_name)
-    @browser.goto 'http://demo.redmine.org'
-    @browser.link(:class, 'register').click
-    @browser.text_field(:id, 'user_login').set login
-    @browser.text_field(:id, 'user_password').set @pass
-    @browser.text_field(:id, 'user_password_confirmation').set @pass
-    @browser.text_field(:id, 'user_firstname').set first_name
-    @browser.text_field(:id, 'user_lastname').set last_name
-    @browser.text_field(:id, 'user_mail').set(login + '@dd.dd')
-    @browser.select_list(:id, "user_language").select 'English'
+# def registration3(login, first_name, last_name)
+#   @browser.goto 'http://demo.redmine.org'
+#   @browser.link(:class, 'register').click
+#   @browser.text_field(:id, 'user_login').set login
+#   @browser.text_field(:id, 'user_password').set @pass
+#   @browser.text_field(:id, 'user_password_confirmation').set @pass
+#   @browser.text_field(:id, 'user_firstname').set first_name
+#   @browser.text_field(:id, 'user_lastname').set last_name
+#   @browser.text_field(:id, 'user_mail').set(login + '@dd.dd')
+#   @browser.select_list(:id, "user_language").select 'English'
+# end
+
+  def hover
+    @browser.goto "https://the-internet.herokuapp.com/hovers"
+    el_list = @browser.elements(:css, ".figure>img")
+    el_list[0].hover
+    @browser.h5(text: 'name: user1').wait_until_present(2)
+    el_list[1].hover
+    @browser.h5(text: 'name: user2').wait_until_present(2)
+    el_list[2].hover
+    @browser.h5(text: 'name: user3').wait_until_present(2)
   end
 
-# browser.goto 'http://demo.redmine.org'
-# browser.link(:class, 'register').click
-# browser.text_field(:id, 'user_login').set @login
-# browser.text_field(:id, 'user_password').set @pass
-# browser.text_field(:id, 'user_password_confirmation').set @pass
-# browser.text_field(:id, 'user_firstname').set 'first_name'
-# browser.text_field(:id, 'user_lastname').set 'last_name'
-# browser.text_field(:id, 'user_mail').set(@login + '@dd.dd')
-# browser.select_list(:id, "user_language").select 'English'
+  def drag_and_drop_realisation
+    @browser.goto "https://the-internet.herokuapp.com/drag_and_drop"
+    el1 = @browser.element(:css, "#column-a")
+    el2 = @browser.element(:css, "#column-b")
+# @browser.driver.action.drag_and_drop(el1, el2).perform
+    el1.driver.action.drag_and_drop(@element, el2.wd).perform
+    sleep 1
+  end
 
-  def test_demo
-    registration3(@login, "first_name", "last_name")
+  def select_list
+    @browser.goto "https://the-internet.herokuapp.com/dropdown"
+    @browser.select_list(:id, "dropdown").select("Option 1")
+    @browser.option(text: 'Option 1').wait_until_present(2)
+    @browser.select_list(:id, "dropdown").select_value("2")
+    @browser.option(text: 'Option 2').wait_until_present(2)
+  end
 
-    @browser.button(:name, 'commit').click
-    @browser.text.include? 'Ваша учётная запись активирована. Вы можете войти.'
+  def iframe_realisation
+    @browser.goto "https://the-internet.herokuapp.com/iframe"
+    @browser.frame(:id => "tinymce").text_field(:id => "tinymce").set "admin"
+  end
+
+  def key_presses
+    @browser.goto "https://the-internet.herokuapp.com/key_presses"
+    @browser.send_keys :enter
+    sleep 1
+    @browser.p(text: 'You entered: ENTER').wait_until_present(2)
+    sleep 1
+    @browser.send_keys :escape
+    sleep 1
+    @browser.p(text: 'You entered: ESCAPE').wait_until_present(2)
+  end
+
+  def jquery_ui_menu
+    @browser.goto "https://the-internet.herokuapp.com/jqueryui/menu"
+    @browser
+  end
+
+  def javascript_alerts
 
   end
+
+  def multiple_windows
+
+  end
+
+  def test_bonustasks
+    # hover
+    # drag_and_drop_realisation
+    # select_list
+    # iframe_realisation
+    # key_presses
+    jquery_ui_menu
+
+
+    # registration3(@login, "first_name", "last_name")
+    # @browser.button(:name, 'commit').click
+    # @browser.text.include? 'Ваша учётная запись активирована. Вы можете войти.'
+
+  end
+
 
   def teardown
     @browser.close
